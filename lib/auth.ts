085d892020d0c9530b37e650002f24a16f357514
sorawitdamkham-ops <sorawitdamkham@gmail.com>
2026-04-27 13:13:52 +0700

    fix: test change authorize

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,49 +2,32 @@
 
 import { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import {prisma} from "@/lib/prisma"
+import { prisma } from "@/lib/prisma"
 import bcrypt from "bcryptjs"
 
 // Next.js ไม่อนุญาตให้ export อย่างอื่นนอกจาก Get Post ตอน deploy จึงจำเป็นต้องมาสร้างใน lib และ import ไปใช้ในไฟล์อื่นๆ
 // คือ [...nextauth]/route.ts
 export const authOptions: AuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        })
-
-        if (!user) return null
-
-        const isValid = await bcrypt.compare(
-          credentials.password,
-          user.password
-        )
-
-        if (!isValid) return null
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/login",
-  },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                return {
+                    id: "1",
+                    email: "test@test.com",
+                    name: "test"
+                }
+            },
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/login",
+    },
 }
